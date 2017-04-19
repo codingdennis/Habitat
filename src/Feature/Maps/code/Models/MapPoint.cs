5c3c60b147c74622bdf7b1e4ef6167930509e4cd
codingdennis <c_hsuan@hotmail.com>
2017-04-20 08:50:15 +1000

    Updated Map feature to use Fortis codegen

--- src/Feature/Maps/code/Models/MapPoint.cs
+++ src/Feature/Maps/code/Models/MapPoint.cs
@@ -1,20 +1,21 @@
 ﻿namespace Sitecore.Feature.Maps.Models
 {
-  public class MapPoint
-  {
-    public MapPoint()
+    using Fortis.Foundation.CodeGen.Templates.Feature.Maps;
+    public class MapPoint
     {
-    }
+        public MapPoint()
+        {
+        }
 
-    public MapPoint(Data.Items.Item item)
-    {
-      this.Name = item[Templates.MapPoint.Fields.Name];
-      this.Address = item[Templates.MapPoint.Fields.Address];
-      this.Location = item[Templates.MapPoint.Fields.Location];
-    }
+        public MapPoint(Data.Items.Item item)
+        {
+            this.Name = item[MapPointItemConstants.Fields.MapPointName.ID];
+            this.Address = item[MapPointItemConstants.Fields.MapPointAddress.ID];
+            this.Location = item[MapPointItemConstants.Fields.MapPointLocation.ID];
+        }
 
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public string Location { get; set; }
-  }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+    }
 }
--- src/Feature/Maps/code/Repositories/MapPointRepository.cs
+++ src/Feature/Maps/code/Repositories/MapPointRepository.cs
@@ -1,46 +1,47 @@
 ﻿namespace Sitecore.Feature.Maps.Repositories
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using Models;
-  using Foundation.SitecoreExtensions.Extensions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+    using Foundation.SitecoreExtensions.Extensions;
+    using Fortis.Foundation.CodeGen.Templates.Feature.Maps;
 
-  public class MapPointRepository : IMapPointRepository
-  {
-    private readonly Foundation.Indexing.Repositories.ISearchServiceRepository searchServiceRepository;
-
-    public MapPointRepository() : this(new Foundation.Indexing.Repositories.SearchServiceRepository(new Foundation.Indexing.Models.SearchSettingsBase { Templates = new[] { Templates.MapPoint.ID } }))
+    public class MapPointRepository : IMapPointRepository
     {
+        private readonly Foundation.Indexing.Repositories.ISearchServiceRepository searchServiceRepository;
+
+        public MapPointRepository() : this(new Foundation.Indexing.Repositories.SearchServiceRepository(new Foundation.Indexing.Models.SearchSettingsBase {Templates = new[] {MapPointItemConstants.TemplateID}}))
+        {
+        }
+
+        public MapPointRepository(Foundation.Indexing.Repositories.ISearchServiceRepository searchServiceRepository)
+        {
+            this.searchServiceRepository = searchServiceRepository;
+        }
+
+        public IEnumerable<MapPoint> GetAll(Data.Items.Item contextItem)
+        {
+            if (contextItem == null)
+            {
+                throw new ArgumentNullException(nameof(contextItem));
+            }
+            if (contextItem.IsDerived(MapPointItemConstants.TemplateID))
+            {
+                return new List<MapPoint>
+                {
+                    new MapPoint(contextItem)
+                };
+            }
+            if (!contextItem.IsDerived(MapPointsFolderItemConstants.TemplateID))
+            {
+                throw new ArgumentException("Item must derive from MapPointsFolder or MapPoint", nameof(contextItem));
+            }
+
+            var searchService = this.searchServiceRepository.Get();
+            searchService.Settings.Root = contextItem;
+
+            return searchService.FindAll().Results.Select(x => new MapPoint(x.Item));
+        }
     }
-
-    public MapPointRepository(Foundation.Indexing.Repositories.ISearchServiceRepository searchServiceRepository)
-    {
-      this.searchServiceRepository = searchServiceRepository;
-    }
-
-    public IEnumerable<MapPoint> GetAll(Data.Items.Item contextItem)
-    {
-      if (contextItem == null)
-      {
-        throw new ArgumentNullException(nameof(contextItem));
-      }
-      if (contextItem.IsDerived(Templates.MapPoint.ID))
-      {
-        return new List<MapPoint>
-               {
-                 new MapPoint(contextItem)
-               };
-      }
-      if (!contextItem.IsDerived(Templates.MapPointsFolder.ID))
-      {
-        throw new ArgumentException("Item must derive from MapPointsFolder or MapPoint", nameof(contextItem));
-      }
-
-      var searchService = this.searchServiceRepository.Get();
-      searchService.Settings.Root = contextItem;
-
-      return searchService.FindAll().Results.Select(x => new MapPoint(x.Item));
-    }
-  }
 }
