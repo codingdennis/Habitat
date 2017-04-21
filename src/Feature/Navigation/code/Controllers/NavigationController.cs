72c056b61a0129545231ea38f712a30f06d03039
codingdennis <c_hsuan@hotmail.com>
2017-04-22 00:13:48 +1000

    Updated Navigation feature to use Fortis

--- src/Feature/Navigation/code/Controllers/NavigationController.cs
+++ src/Feature/Navigation/code/Controllers/NavigationController.cs
@@ -1,63 +1,67 @@
 ﻿namespace Sitecore.Feature.Navigation.Controllers
 {
-  using System.Web.Mvc;
-  using Sitecore.Feature.Navigation.Repositories;
-  using Sitecore.Foundation.Alerts.Extensions;
-  using Sitecore.Foundation.Alerts.Models;
-  using Sitecore.Foundation.Dictionary.Repositories;
-  using Sitecore.Mvc.Presentation;
+    using System.Web.Mvc;
+    using Sitecore.Feature.Navigation.Repositories;
+    using Sitecore.Foundation.Alerts.Extensions;
+    using Sitecore.Foundation.Alerts.Models;
+    using Sitecore.Foundation.Dictionary.Repositories;
+    using Fortis.Foundation.CodeGen.Templates.Project.Common.ContentTypes;
+    using Fortis.Model;
+    using IItemFactory = Fortis.Model.IItemFactory;
 
-  public class NavigationController : Controller
-  {
-    private readonly INavigationRepository _navigationRepository;
-
-    public NavigationController() : this(new NavigationRepository(RenderingContext.Current.ContextItem))
+    public class NavigationController : Controller
     {
-    }
+        private readonly INavigationRepository _navigationRepository;
+        private readonly IItemFactory _itemFactory;
 
-    public NavigationController(INavigationRepository navigationRepository)
-    {
-      this._navigationRepository = navigationRepository;
-    }
+        public NavigationController() : this(new NavigationRepository(Mvc.Presentation.RenderingContext.Current.ContextItem, DependencyResolver.Current.GetService<IItemFactory>()))
+        {
+        }
 
-    public ActionResult Breadcrumb()
-    {
-      var items = this._navigationRepository.GetBreadcrumb();
-      return this.View("Breadcrumb", items);
-    }
+        public NavigationController(INavigationRepository navigationRepository)
+        {
+            this._navigationRepository = navigationRepository;
+            this._itemFactory = navigationRepository.ItemFactory;
+        }
 
-    public ActionResult PrimaryMenu()
-    {
-      var items = this._navigationRepository.GetPrimaryMenu();
-      return this.View("PrimaryMenu", items);
-    }
+        public ActionResult Breadcrumb()
+        {
+            var items = this._navigationRepository.GetBreadcrumb();
+            return this.View("Breadcrumb", items);
+        }
 
-    public ActionResult SecondaryMenu()
-    {
-      var item = this._navigationRepository.GetSecondaryMenuItem();
-      return this.View("SecondaryMenu", item);
-    }
+        public ActionResult PrimaryMenu()
+        {
+            var items = this._navigationRepository.GetPrimaryMenu();
+            return this.View("PrimaryMenu", items);
+        }
 
-    public ActionResult NavigationLinks()
-    {
-      if (string.IsNullOrEmpty(RenderingContext.Current.Rendering.DataSource))
-      {
-        return null;
-      }
-      var item = RenderingContext.Current.Rendering.Item;
-      var items = this._navigationRepository.GetLinkMenuItems(item);
-      return this.View(items);
-    }
+        public ActionResult SecondaryMenu()
+        {
+            var item = this._navigationRepository.GetSecondaryMenuItem();
+            return this.View("SecondaryMenu", item);
+        }
 
-    public ActionResult LinkMenu()
-    {
-      if (string.IsNullOrEmpty(RenderingContext.Current.Rendering.DataSource))
-      {
-        return Context.PageMode.IsExperienceEditor ? this.InfoMessage(new InfoMessage(DictionaryPhraseRepository.Current.Get("/Navigation/Link Menu/No Items", "This menu has no items."), InfoMessage.MessageType.Warning)) : null;
-      }
-      var item = RenderingContext.Current.Rendering.Item;
-      var items = this._navigationRepository.GetLinkMenuItems(item);
-      return this.View("LinkMenu", items);
+        public ActionResult NavigationLinks()
+        {
+            if (string.IsNullOrEmpty(Mvc.Presentation.RenderingContext.Current.Rendering.DataSource))
+            {
+                return null;
+            }
+            var renderingModel = this._itemFactory.GetRenderingContextItems<IItemWrapper, ILinkMenu>();
+            var items = this._navigationRepository.GetLinkMenuItems(renderingModel.RenderingItem);
+            return this.View(items);
+        }
+
+        public ActionResult LinkMenu()
+        {
+            if (string.IsNullOrEmpty(Mvc.Presentation.RenderingContext.Current.Rendering.DataSource))
+            {
+                return Context.PageMode.IsExperienceEditor ? this.InfoMessage(new InfoMessage(DictionaryPhraseRepository.Current.Get("/Navigation/Link Menu/No Items", "This menu has no items."), InfoMessage.MessageType.Warning)) : null;
+            }
+            var renderingModel = this._itemFactory.GetRenderingContextItems<IItemWrapper, ILinkMenu>();
+            var items = this._navigationRepository.GetLinkMenuItems(renderingModel.RenderingItem);
+            return this.View("LinkMenu", items);
+        }
     }
-  }
 }
--- src/Feature/Navigation/code/Repositories/INavigationRepository.cs
+++ src/Feature/Navigation/code/Repositories/INavigationRepository.cs
@@ -1,14 +1,18 @@
 namespace Sitecore.Feature.Navigation.Repositories
 {
-  using Sitecore.Data.Items;
-  using Sitecore.Feature.Navigation.Models;
+    using Sitecore.Data.Items;
+    using Sitecore.Feature.Navigation.Models;
+    using Fortis.Foundation.CodeGen.Templates.Feature.Navigation;
+    using Fortis.Foundation.CodeGen.Templates.Project.Common.ContentTypes;
+    using Fortis.Model;
 
-  public interface INavigationRepository
-  {
-    Item GetNavigationRoot(Item contextItem);
-    NavigationItems GetBreadcrumb();
-    NavigationItems GetPrimaryMenu();
-    NavigationItem GetSecondaryMenuItem();
-    NavigationItems GetLinkMenuItems(Item menuItem);
-  }
+    public interface INavigationRepository
+    {
+        IItemFactory ItemFactory { get; }
+        INavigationRoot GetNavigationRoot();
+        NavigationItems GetBreadcrumb();
+        NavigationItems GetPrimaryMenu();
+        NavigationItem GetSecondaryMenuItem();
+        NavigationItems GetLinkMenuItems(ILinkMenu menuItem);
+    }
 }
