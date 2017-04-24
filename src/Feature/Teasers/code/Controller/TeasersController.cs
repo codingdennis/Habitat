002461b758fffd93cd07cea06e97f38a5b65ef02
codingdennis <c_hsuan@hotmail.com>
2017-04-24 15:20:02 +1000

    Updated Teaser feature to use Fortis (except the dynamic model which will leave as-is).

--- src/Feature/Teasers/code/Controller/TeasersController.cs
+++ src/Feature/Teasers/code/Controller/TeasersController.cs
@@ -1,33 +1,34 @@
 ﻿namespace Sitecore.Feature.Teasers.Controller
 {
-  using System.Web.Mvc;
-  using Sitecore.Feature.Teasers.Models;
-  using Sitecore.Foundation.Alerts;
-  using Sitecore.Foundation.Alerts.Extensions;
-  using Sitecore.Foundation.Alerts.Models;
-  using Sitecore.Foundation.SitecoreExtensions.Extensions;
-  using Sitecore.Mvc.Presentation;
+    using System.Web.Mvc;
+    using Sitecore.Feature.Teasers.Models;
+    using Sitecore.Foundation.Alerts;
+    using Sitecore.Foundation.Alerts.Extensions;
+    using Sitecore.Foundation.Alerts.Models;
+    using Sitecore.Foundation.SitecoreExtensions.Extensions;
+    using Sitecore.Mvc.Presentation;
+    using Fortis.Foundation.CodeGen.Templates.Feature.Teasers;
 
-  public class TeasersController : Controller
-  {
-    public ActionResult GetDynamicContent(string viewName)
+    public class TeasersController : Controller
     {
-      var dataSourceItem = RenderingContext.Current.Rendering.Item;
-      if (!dataSourceItem?.IsDerived(Templates.DynamicTeaser.ID) ?? true)
-      {
-        return Context.PageMode.IsExperienceEditor ? this.InfoMessage(new InfoMessage(AlertTexts.InvalidDataSourceTemplateFriendlyMessage, InfoMessage.MessageType.Error)) : null;
-      }
+        public ActionResult GetDynamicContent(string viewName)
+        {
+            var dataSourceItem = RenderingContext.Current.Rendering.Item;
+            if (!dataSourceItem?.IsDerived(DynamicTeaserConstants.TemplateID) ?? true)
+            {
+                return Context.PageMode.IsExperienceEditor ? this.InfoMessage(new InfoMessage(AlertTexts.InvalidDataSourceTemplateFriendlyMessage, InfoMessage.MessageType.Error)) : null;
+            }
 
-      var model = new DynamicTeaserModel(dataSourceItem);
-      return this.View(viewName, model);
+            var model = new DynamicTeaserModel(dataSourceItem);
+            return this.View(viewName, model);
+        }
+
+        public ActionResult Accordion() => this.GetDynamicContent("Accordion");
+
+        public ActionResult Tabs() => this.GetDynamicContent("Tabs");
+
+        public ActionResult TeaserCarousel() => this.GetDynamicContent("TeaserCarousel");
+
+        public ActionResult JumbotronCarousel() => this.GetDynamicContent("JumbotronCarousel");
     }
-
-    public ActionResult Accordion() => this.GetDynamicContent("Accordion");
-
-    public ActionResult Tabs() => this.GetDynamicContent("Accordion");
-
-    public ActionResult TeaserCarousel() => this.GetDynamicContent("TeaserCarousel");
-
-    public ActionResult JumbotronCarousel() => this.GetDynamicContent("JumbotronCarousel");
-  }
 }
--- src/Feature/Teasers/code/Models/DynamicTeaserItem.cs
+++ src/Feature/Teasers/code/Models/DynamicTeaserItem.cs
@@ -1,41 +1,42 @@
 ﻿namespace Sitecore.Feature.Teasers.Models
 {
-  using System;
-  using Sitecore.Data.Fields;
-  using Sitecore.Data.Items;
-  using Sitecore.Foundation.SitecoreExtensions.Extensions;
+    using System;
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+    using Sitecore.Foundation.SitecoreExtensions.Extensions;
+    using Fortis.Foundation.CodeGen.Templates.Feature.Teasers;
 
     public class DynamicTeaserItem
-  {
-    public DynamicTeaserItem(Item headline) : this()
     {
-      this.Item = headline;
-      this.Title = headline[Templates.TeaserHeadline.Fields.Title];
-      this.Icon = this.GetIcon(headline);
+        public DynamicTeaserItem(Item headline) : this()
+        {
+            this.Item = headline;
+            this.Title = headline[HeadlineConstants.Fields.TeaserTitle.ID];
+            this.Icon = this.GetIcon(headline);
+        }
+
+        private string GetIcon(Item headline)
+        {
+            return headline?.TargetItem(HeadlineConstants.Fields.TeaserIcon.ID)?[IconConstants.Fields.CssClass.ID];
+        }
+
+        public DynamicTeaserItem()
+        {
+            this.HeaderId = $"header{Guid.NewGuid().ToString("N")}";
+            this.PanelId = $"panel{Guid.NewGuid().ToString("N")}";
+        }
+
+        [CanBeNull]
+        public Item Item { get; set; }
+
+        [CanBeNull]
+        public string Title { get; private set; }
+
+        [CanBeNull]
+        public string Icon { get; set; }
+
+        public bool IsActive { get; set; }
+        public string HeaderId { get; private set; }
+        public string PanelId { get; private set; }
     }
-
-    private string GetIcon(Item headline)
-    {
-      return headline?.TargetItem(Templates.TeaserHeadline.Fields.Icon)?[Templates.Icon.Fields.CssClass];
-    }
-
-    public DynamicTeaserItem()
-    {
-      this.HeaderId = $"header{Guid.NewGuid().ToString("N")}";
-      this.PanelId = $"panel{Guid.NewGuid().ToString("N")}";
-    }
-
-    [CanBeNull]
-    public Item Item { get; set; }
-
-    [CanBeNull]
-    public string Title { get; private set; }
-
-    [CanBeNull]
-    public string Icon { get; set; }
-
-    public bool IsActive { get; set; }
-    public string HeaderId { get; private set; }
-    public string PanelId { get; private set; }
-  }
 }
