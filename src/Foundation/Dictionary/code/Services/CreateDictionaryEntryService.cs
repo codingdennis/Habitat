94416df173a9379671d846e5ae23fee31eb88a0c
codingdennis <c_hsuan@hotmail.com>
2017-04-27 14:27:13 +1000

    Updated dictionary foundation module to use Fortis codegen

--- src/Foundation/Dictionary/code/Services/CreateDictionaryEntryService.cs
+++ src/Foundation/Dictionary/code/Services/CreateDictionaryEntryService.cs
@@ -3,62 +3,63 @@ using System.Linq;
 
 namespace Sitecore.Foundation.Dictionary.Services
 {
-  using Sitecore.Data;
-  using Sitecore.Data.Items;
-  using Sitecore.Foundation.Dictionary.Models;
-  using Sitecore.SecurityModel;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Foundation.Dictionary.Models;
+    using Sitecore.SecurityModel;
+    using Fortis.Foundation.CodeGen.Templates.Foundation.Dictionary;
 
-  internal static class CreateDictionaryEntryService
-  {
-    public static Item CreateDictionaryEntry(Dictionary dictionary, string relativePath, string defaultValue)
+    internal static class CreateDictionaryEntryService
     {
-      lock (dictionary)
-      {
-        var parts = relativePath.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-        var root = dictionary.Root;
-        for (var i = 0; i < parts.Length - 1; i++)
+        public static Item CreateDictionaryEntry(Dictionary dictionary, string relativePath, string defaultValue)
         {
-          root = CreateDictionaryFolder(parts[i], root);
+            lock (dictionary)
+            {
+                var parts = relativePath.Split(new[] {'/', '\\'}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var root = dictionary.Root;
+                for (var i = 0; i < parts.Length - 1; i++)
+                {
+                    root = CreateDictionaryFolder(parts[i], root);
+                }
+                return CreateDictionaryEntry(parts.Last(), root, defaultValue);
+            }
         }
-        return CreateDictionaryEntry(parts.Last(), root, defaultValue);
-      }
-    }
 
-    private static Item CreateDictionaryEntry(string name, Item root, string defaultValue)
-    {
-      using (new SecurityDisabler())
-      {
-        var item = GetOrCreateDictionaryItem(name, root, Templates.DictionaryEntry.ID);
-        using (new EditContext(item))
+        private static Item CreateDictionaryEntry(string name, Item root, string defaultValue)
         {
-          item[Templates.DictionaryEntry.Fields.Phrase] = defaultValue;
+            using (new SecurityDisabler())
+            {
+                var item = GetOrCreateDictionaryItem(name, root, DictionaryEntryConstants.TemplateID);
+                using (new EditContext(item))
+                {
+                    item[DictionaryEntryConstants.Fields.Phrase.ID] = defaultValue;
+                }
+                return item;
+            }
         }
-        return item;
-      }
-    }
 
-    private static Item CreateDictionaryFolder(string name, Item parent)
-    {
-      using (new SecurityDisabler())
-      {
-        return GetOrCreateDictionaryItem(name, parent, Templates.DictionaryFolder.ID);
-      }
-    }
+        private static Item CreateDictionaryFolder(string name, Item parent)
+        {
+            using (new SecurityDisabler())
+            {
+                return GetOrCreateDictionaryItem(name, parent, DictionaryFolderConstants.TemplateID);
+            }
+        }
 
-    private static Item GetOrCreateDictionaryItem(string name, Item parent, ID templateID)
-    {
-      var item = parent.Axes.GetChild(name);
-      if (item != null)
-        return item;
+        private static Item GetOrCreateDictionaryItem(string name, Item parent, ID templateID)
+        {
+            var item = parent.Axes.GetChild(name);
+            if (item != null)
+                return item;
 
-      try
-      {
-        return parent.Add(name, new TemplateID(templateID));
-      }
-      catch (Exception ex)
-      {
-        throw new InvalidOperationException($"Could not create item {name} under {parent.Name}", ex);
-      }
+            try
+            {
+                return parent.Add(name, new TemplateID(templateID));
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Could not create item {name} under {parent.Name}", ex);
+            }
+        }
     }
-  }
 }
