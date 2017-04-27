c1ef7ddb89f754eb00c72facead23a6d9e0160b1
codingdennis <c_hsuan@hotmail.com>
2017-04-27 17:42:38 +1000

    Updated references to constants

--- src/Feature/Language/Tests/Extensions/AutoLanguageDbDataAttribute.cs
+++ src/Feature/Language/Tests/Extensions/AutoLanguageDbDataAttribute.cs
@@ -5,13 +5,14 @@ namespace Sitecore.Feature.Language.Tests.Extensions
     using Sitecore.Foundation.Multisite;
     using Sitecore.Foundation.Testing.Attributes;
     using Fortis.Foundation.CodeGen.Templates.Feature.Language;
+    using Fortis.Foundation.CodeGen.Templates.Foundation.Multisite;
 
     public class AutoLanguageDbDataAttribute : AutoDbDataAttribute
     {
         public AutoLanguageDbDataAttribute()
         {
             var db = this.Fixture.Create<Db>();
-            db.Add(new DbTemplate(Templates.Site.ID));
+            db.Add(new DbTemplate(SiteConstants.TemplateID));
             db.Add(new DbTemplate(LanguageSettingsConstants.TemplateID)
             {
                 Fields =
--- src/Feature/Language/Tests/LanguageRepositoryTests.cs
+++ src/Feature/Language/Tests/LanguageRepositoryTests.cs
@@ -1,103 +1,104 @@
 ﻿namespace Sitecore.Feature.Language.Tests
 {
-  using System.Linq;
-  using FluentAssertions;
-  using Fortis.Foundation.CodeGen.Templates.Feature.Language;
-  using Sitecore.Collections;
-  using Sitecore.Data;
-  using Sitecore.FakeDb;
-  using Sitecore.FakeDb.AutoFixture;
-  using Sitecore.FakeDb.Sites;
-  using Sitecore.Feature.Language.Repositories;
-  using Sitecore.Feature.Language.Tests.Extensions;
-  using Sitecore.Foundation.Multisite;
-  using Sitecore.Foundation.Testing.Attributes;
-  using Sitecore.Sites;
-  using Xunit;
+    using System.Linq;
+    using FluentAssertions;
+    using Sitecore.Collections;
+    using Sitecore.Data;
+    using Sitecore.FakeDb;
+    using Sitecore.FakeDb.AutoFixture;
+    using Sitecore.FakeDb.Sites;
+    using Sitecore.Feature.Language.Repositories;
+    using Sitecore.Feature.Language.Tests.Extensions;
+    using Sitecore.Foundation.Multisite;
+    using Sitecore.Foundation.Testing.Attributes;
+    using Sitecore.Sites;
+    using Xunit;
+    using Fortis.Foundation.CodeGen.Templates.Feature.Language;
+    using Fortis.Foundation.CodeGen.Templates.Foundation.Multisite;
 
-  public class LanguageRepositoryTests
-  {
-    [Theory]
-    [AutoDbData]
-    public void GetActive_ShouldReturnLanguageModelForContextLanguage(Db db, [Content] DbItem item)
+    public class LanguageRepositoryTests
     {
-      var contextItem = db.GetItem(item.ID);
-      Context.Item = contextItem;
-      var activeLanguage = LanguageRepository.GetActive();
-      activeLanguage.TwoLetterCode.Should().BeEquivalentTo(Context.Language.Name);
-    }
-
-    [Theory]
-    [AutoLanguageDbData]
-    public void GetSupportedLanguages_OneSelected_ShouldReturnSelected(Db db, [Content] DbTemplate template, DbItem item, string rootName)
-    {
-      template.BaseIDs = new[]
-      {
-        Templates.Site.ID, LanguageSettingsConstants.TemplateID
-      };
-
-      var languageItem = new DbItem("en");
-      db.Add(languageItem);
-
-
-      var siteRootItem = new DbItem(rootName, ID.NewID, template.ID)
-      {
-        new DbField(LanguageSettingsConstants.Fields.SupportedLanguages.ID)
+        [Theory]
+        [AutoDbData]
+        public void GetActive_ShouldReturnLanguageModelForContextLanguage(Db db, [Content] DbItem item)
         {
-          {
-            "en", languageItem.ID.ToString()
-          }
+            var contextItem = db.GetItem(item.ID);
+            Context.Item = contextItem;
+            var activeLanguage = LanguageRepository.GetActive();
+            activeLanguage.TwoLetterCode.Should().BeEquivalentTo(Context.Language.Name);
         }
-      };
 
-      siteRootItem.Add(item);
-      db.Add(siteRootItem);
-      var contextItem = db.GetItem(item.ID);
-      Context.Item = contextItem;
-      var supportedLanguages = LanguageRepository.GetSupportedLanguages();
-      supportedLanguages.Count().Should().BeGreaterThan(0);
-    }
-
-    [Theory]
-    [AutoLanguageDbData]
-    public void GetSupportedLanguages_NoneSelected_ShouldReturnEmptyList(Db db, [Content] DbTemplate template, DbItem item, string rootName)
-    {
-      template.BaseIDs = new[]
-      {
-        Templates.Site.ID, LanguageSettingsConstants.TemplateID
-      };
-
-      var languageItem = new DbItem("en");
-      db.Add(languageItem);
-
-      var siteRootItem = new DbItem(rootName, ID.NewID, template.ID)
-      {
-        new DbField(LanguageSettingsConstants.Fields.SupportedLanguages.ID)
+        [Theory]
+        [AutoLanguageDbData]
+        public void GetSupportedLanguages_OneSelected_ShouldReturnSelected(Db db, [Content] DbTemplate template, DbItem item, string rootName)
         {
-          {
-            "en", ""
-          }
+            template.BaseIDs = new[]
+            {
+                SiteConstants.TemplateID, LanguageSettingsConstants.TemplateID
+            };
+
+            var languageItem = new DbItem("en");
+            db.Add(languageItem);
+
+
+            var siteRootItem = new DbItem(rootName, ID.NewID, template.ID)
+            {
+                new DbField(LanguageSettingsConstants.Fields.SupportedLanguages.ID)
+                {
+                    {
+                        "en", languageItem.ID.ToString()
+                    }
+                }
+            };
+
+            siteRootItem.Add(item);
+            db.Add(siteRootItem);
+            var contextItem = db.GetItem(item.ID);
+            Context.Item = contextItem;
+            var supportedLanguages = LanguageRepository.GetSupportedLanguages();
+            supportedLanguages.Count().Should().BeGreaterThan(0);
         }
-      };
 
-      siteRootItem.Add(item);
-      db.Add(siteRootItem);
-      var contextItem = db.GetItem(item.ID);
-      Context.Item = contextItem;
+        [Theory]
+        [AutoLanguageDbData]
+        public void GetSupportedLanguages_NoneSelected_ShouldReturnEmptyList(Db db, [Content] DbTemplate template, DbItem item, string rootName)
+        {
+            template.BaseIDs = new[]
+            {
+                SiteConstants.TemplateID, LanguageSettingsConstants.TemplateID
+            };
 
-      var fakeSite = new FakeSiteContext(new StringDictionary()
-      {
-        { "name", "fake" },
-        { "database", "master" },
-        {"rootPath", siteRootItem.FullPath },
-        { "hostName", "local" }
-      });
+            var languageItem = new DbItem("en");
+            db.Add(languageItem);
 
-      using (new FakeSiteContextSwitcher(fakeSite))
-      {
-        var supportedLanguages = LanguageRepository.GetSupportedLanguages();
-        supportedLanguages.Count().Should().Be(0);
-      }
+            var siteRootItem = new DbItem(rootName, ID.NewID, template.ID)
+            {
+                new DbField(LanguageSettingsConstants.Fields.SupportedLanguages.ID)
+                {
+                    {
+                        "en", ""
+                    }
+                }
+            };
+
+            siteRootItem.Add(item);
+            db.Add(siteRootItem);
+            var contextItem = db.GetItem(item.ID);
+            Context.Item = contextItem;
+
+            var fakeSite = new FakeSiteContext(new StringDictionary()
+            {
+                {"name", "fake"},
+                {"database", "master"},
+                {"rootPath", siteRootItem.FullPath},
+                {"hostName", "local"}
+            });
+
+            using (new FakeSiteContextSwitcher(fakeSite))
+            {
+                var supportedLanguages = LanguageRepository.GetSupportedLanguages();
+                supportedLanguages.Count().Should().Be(0);
+            }
+        }
     }
-  }
 }
